12bf4f8381b161028150190648195829d10ad375
Christopher Szafranski <christopherszafranski@unknown.local>
2017-09-23 17:09:11 -0400

    updated config options

--- cse303/cse303.p2/config/auth.js
+++ cse303/cse303.p2/config/auth.js
@@ -3,9 +3,9 @@
 // clientID and clientSecret, and then you should be good to go.
 module.exports = {
     'googleAuth' : {
-        'clientID'      : '', // TODO: put your client ID, from Google Developer Console
-        'clientSecret'  : '', // TODO: put your client secret, from Google Developer Console
-        'callbackURL'   : ''  // TODO: you need to use your AWS IP and port, e.g., 'http://100.2.66.4:8080/auth/google/callback'
+        'clientID'      : '1052006320858-q0gli3iep1ag20slrsenitadcds8h79a.apps.googleusercontent.com', // TODO: put your client ID, from Google Developer Console
+        'clientSecret'  : 'pTJ-m2AW9upD2Ak429BoGq4A', // TODO: put your client secret, from Google Developer Console
+        'callbackURL'   : 'http://54.88.147.196:9000/auth/google/callback'  // TODO: you need to use your AWS IP and port, e.g., 'http://100.2.66.4:8080/auth/google/callback'
     },
     // When this next field is true, user account creation is enabled.  Note
     // that the whole web app doesn't have local state.  You can set this
--- cse303/cse303.p2/config/mcd.js
+++ cse303/cse303.p2/config/mcd.js
@@ -2,5 +2,6 @@
 // information in the main code folders, but we *do* want it in the
 // repository for fast deployment.
 module.exports = {
-    'url' : '' // TODO: put the path to your memcached instance, such as 10.2.5.6:11211
+    'url' : '54.226.72.162' // TODO: put the path to your memcached instance, such as 10.2.5.6:11211
+    //Public IP mcd EC2 instance: 54.226.72.162
 }
