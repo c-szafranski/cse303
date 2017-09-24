d64c847b8f1758d848020649a3f7582c448154a7
Christopher Szafranski <christopherszafranski@unknown.local>
2017-09-23 20:13:05 -0400

    Added db url

--- cse303/cse303.p2/config/database.js
+++ cse303/cse303.p2/config/database.js
@@ -2,5 +2,5 @@
 // information in the main code folders, but we *do* want it in the
 // repository for fast deployment.
 module.exports = {
-    'url'   : '' // TODO: put the path to your database, such as 'mongodb://100.2.5.6/mydatabase'
+    'url'   : 'mongodb://34.230.10.202/survey/' // TODO: put the path to your database, such as 'mongodb://100.2.5.6/mydatabase'
 };
